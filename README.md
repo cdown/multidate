95d10412dd7a88b6434b02f825cc59fd53716326
Chris Down <chris@chrisdown.name>
2023-06-09 09:39:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# multidate
+
+multidate prints multiple timezones' dates/times with offsets from the local
+time. Useful for people who work in global teams and often need to answer what
+time it is somewhere else.
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use chrono::{DateTime, Duration, Local, Offset, TimeZone, Utc};
+use chrono_tz::Tz;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Config {
+    #[arg(help = "Remote timezone(s) to print, for example, Europe/London")]
+    tz: Vec<Tz>,
+
+    #[arg(
+        short,
+        long,
+        help = "A strftime format specifying how to format times",
+        default_value = "%a %F %H:%M"
+    )]
+    fmt: String,
+}
+
+struct RemoteTz {
+    tz: Tz,
+    offset_from_local: Duration,
+}
+
+fn format_duration(duration: &Duration) -> String {
+    let hours = duration.num_hours();
+    let minutes = duration.num_minutes() % 60;
+
+    if hours == 0 && minutes == 0 {
+        "".to_string()
+    } else if minutes == 0 {
+        format!("{:+}h", hours)
+    } else {
+        format!("{:+}h{:+02}m", hours, minutes)
+    }
+}
+
+fn get_remote_tz(local_offset_secs: i32, tz: Tz) -> RemoteTz {
+    let offset_from_local_secs = tz
+        .offset_from_utc_datetime(&Utc::now().naive_utc())
+        .fix()
+        .local_minus_utc()
+        - local_offset_secs;
+    let offset_from_local = Duration::seconds(offset_from_local_secs.into());
+
+    RemoteTz {
+        tz,
+        offset_from_local,
+    }
+}
+
+fn main() {
+    let cfg = Config::parse();
+    let local: DateTime<Local> = Local::now();
+    let local_offset = local.offset().fix().local_minus_utc();
+
+    let remote_tzs: Vec<_> = cfg
+        .tz
+        .into_iter()
+        .map(|tz| get_remote_tz(local_offset, tz))
+        .collect();
+
+    println!("Local: {}", local.format(&cfg.fmt),);
+
+    for rtz in remote_tzs {
+        println!(
+            "{}: {} {}",
+            rtz.tz,
+            local.with_timezone(&rtz.tz).format(&cfg.fmt),
+            format_duration(&rtz.offset_from_local),
+        );
+    }
+}
